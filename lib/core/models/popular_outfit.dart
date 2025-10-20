3923c187e3310fe6e1a08e835f609cd30926b869
OHsujin <tnwls2dkdlel@naver.com>
2025-10-20 21:47:31 +0900

    feat: 인기 착장 모델에 아우터 필드 추가
    - outerwear 옵션 필드 추가 (nullable)
    - buildRecommendationMessage()에서 아우터 포함한 조합 메시지 생성
    - 2개 아이템: "{상의}와 {하의}" 형식
    - 3개 아이템: "{상의}, {하의}, {아우터}" 형식

--- lib/core/models/popular_outfit.dart
+++ lib/core/models/popular_outfit.dart
@@ -3,6 +3,7 @@ class PopularOutfit {
   const PopularOutfit({
     required this.top,
     required this.bottom,
+    this.outerwear,
     required this.count,
     required this.cityName,
   });
@@ -13,6 +14,9 @@ class PopularOutfit {
   /// 하의 이름
   final String bottom;
 
+  /// 아우터 이름 (선택사항)
+  final String? outerwear;
+
   /// 해당 조합을 입은 사용자 수
   final int count;
 
@@ -21,13 +25,28 @@ class PopularOutfit {
 
   /// 추천 문구 생성
   String buildRecommendationMessage() {
-    return '$cityName 유저들이 가장 많이 입은 착장은 $top와 $bottom 조합이에요! 👍';
+    final items = <String>[top, bottom];
+    if (outerwear != null && outerwear!.isNotEmpty) {
+      items.add(outerwear!);
+    }
+
+    String itemsText;
+    if (items.length == 2) {
+      itemsText = '${items[0]}와 ${items[1]}';
+    } else if (items.length == 3) {
+      itemsText = '${items[0]}, ${items[1]}, ${items[2]}';
+    } else {
+      itemsText = items.join(', ');
+    }
+
+    return '$cityName 유저들이 가장 많이 입은 착장은 $itemsText 조합이에요! 👍';
   }
 
   factory PopularOutfit.fromJson(Map<String, dynamic> json) {
     return PopularOutfit(
       top: json['top'] as String,
       bottom: json['bottom'] as String,
+      outerwear: json['outerwear'] as String?,
       count: json['count'] as int,
       cityName: json['city_name'] as String,
     );
